2b8cceb5809e88c41528bc43f2dd400d5cc7d833
Niloy <nafis.sadique.niloy@g.bracu.ac.bd>
2025-04-11 10:46:32 +0600

    add buttons with click handlers to display alerts

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@
 import './App.css'
 
 function App() {
-  
+  function handleCLick() {
+    alert('Hello Niloy')
+  }
+  const handleClick3 =()=>{
+    alert('Hello Niloy3')
+  }
   return (
     <>
       <h1>Niloy coding</h1>
+      <button onClick={handleCLick}>Click</button>
+
+      <button onClick={function handleCLick2(){
+        alert('Hello Niloy2')
+      }}>Click2</button>
+      
+      <button onClick={handleClick3}>Click Me3</button>
     </>
   )
 }
